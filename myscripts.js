3499d897463fe6941623539582008e89e359bb56
Horatio Caine <horatio.caine@hotmail.co.uk>
2022-10-13 13:01:21 +0900

    ADD UI buttons, restart function and score tracker

--- myscripts.js
+++ myscripts.js
@@ -1,75 +1,123 @@
-function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-    console.log(computerSelection);    
-    return computerSelection;
-}
-
-
-   
- 
 let playerScore = 0;
 let computerScore = 0;
 
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const playerResult = document.querySelector('#playerResult');
+const computerResult = document.querySelector('#computerResult');
+const gameResult = document.querySelector('#gameResult');
+const endGame = document.querySelector('#endGame');
+const playAgain = document.querySelector('.playAgain');
+const restartGame = document.querySelector('#restartGame');
+
+rock.addEventListener("click", function() {
+    let playerSelection = "rock";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+paper.addEventListener("click", function() {
+    let playerSelection = "paper";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+scissors.addEventListener("click", function() {
+    let playerSelection = "scissors";
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+playAgain.addEventListener("click", function() {
+    playerScore = 0;
+    computerScore = 0;
+    rock.style.display = "block";
+    rock.style.visibility = "visible";
+    paper.style.display = "block";
+    paper.style.visibility = "visible";
+    scissors.style.display = "block";
+    scissors.style.visibility = "visible";
+    playAgain.style.display = "none";
+    playAgain.style.visibility = "hidden";
+    endGame.textContent = "";
+    restartGame.textContent = "";
+    computerResult.textContent = "Computer score: " + computerScore;
+    playerResult.textContent = "Your score: " + playerScore;
+});
+
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
+    return computerSelection;
+}
+
 function playRound(playerSelection, computerSelection) {
-    
-    
     if (playerSelection === "rock" && computerSelection === "Paper") {
         computerScore++;
-        return "You lose! Paper beats rock!";
+        computerResult.textContent = "Computer score: " + computerScore;
+        gameResult.textContent = "You lose! You selected rock and the computer selected paper!";
     } else if (playerSelection === "rock" && computerSelection === "Rock") {
-        return "It's a draw!";
+        gameResult.textContent = "It's a draw! You and the computer both selected rock!";
     } else if (playerSelection === "rock" && computerSelection === "Scissors") {
         playerScore++;
-        return "You win! Rock beats scissors!";
+        playerResult.textContent = "Your score: " + playerScore;
+        gameResult.textContent = "You win! You selected rock and the computer selected scissors!"
     } else if (playerSelection === "paper" && computerSelection === "Paper") {
-        return "It's a draw!"; 
+        gameResult.textContent = "It's a draw! You and the computer both selected paper!";
     } else if (playerSelection === "paper" && computerSelection === "Rock") {
         playerScore++;
-        return "You win! Paper beats rock!";
+        playerResult.textContent = "Your score: " + playerScore;
+        gameResult.textContent = "You win! You selected paper and the computer selected rock!";
     } else if (playerSelection === "paper" && computerSelection === "Scissors") {
         computerScore++;
-        return "You lose! Scissors beats paper!";
+        computerResult.textContent = "Computer score: " + computerScore;
+        gameResult.textContent = "You lose! You selected paper and the computer selected scissors!";
     } else if (playerSelection === "scissors" && computerSelection === "Paper") {
         playerScore++;
-        return "You win! Scissors beat paper!";
+        playerResult.textContent = "Your score: " + playerScore;
+        gameResult.textContent = "You win! You selected scissors and the computer selected paper!";
     } else if (playerSelection === "scissors" && computerSelection === "Rock") {
         computerScore++;
-        return "You lose! Rock beats scissors!";
+        computerResult.textContent = "Computer " + computerScore;
+        gameResult.textContent = "You lose! You selected scissors and the computer selected rock!";
     } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
-        return "It's a draw!";
-    }
-    
-}
-
-
-function game() {
-    
-for (let i = 0; i < 5; i++) {
-    playRound();
-
-    let playerSelection = prompt("Rock, paper, or scissors?");
-
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection))
-    console.log(playerSelection)
-    console.log("your score = " + playerScore);
-    console.log("Computer's score = " + computerScore);
+        gameResult.textContent = "It's a draw! You and the computer both selected scissors!";
+    } 
+    if (playerScore === 5 || computerScore === 5 ) {
+        end();
     }
 }
-
-function endGame() {
-
-    if (playerScore > computerScore) {
-        console.log("You win!")
-    } else {
-        console.log("You lose!")
-    }
-}    
-        
-game()
-endGame()
-     
   
-
-
+function end() {
+    if (computerScore === 5 ) {
+        gameResult.textContent = "";
+        computerResult.textContent = "";
+        playerResult.textContent = "";
+        rock.style.display = "none";
+        rock.style.visibility = "hidden";
+        paper.style.display = "none";
+        paper.style.visibility = "hidden";
+        scissors.style.display = "none";
+        scissors.style.visibility = "hidden";
+        endGame.textContent = "You lose! The computer beat you five times. Better luck next time!";
+        playAgain.style.display = "block";
+        playAgain.style.visibility = "visible";
+        restartGame.textContent = "Press the image to restart the game.";
+    }
+    else {
+        gameResult.textContent = "";
+        computerResult.textContent = "";
+        playerResult.textContent = "";
+        rock.style.display = "none";
+        rock.style.visibility = "hidden";
+        paper.style.display = "none";
+        paper.style.visibility = "hidden";
+        scissors.style.display = "none";
+        scissors.style.visibility = "hidden";
+        endGame.textContent = "You win! You beat the computer five times. Well done!";
+        playAgain.style.display = "block";
+        playAgain.style.visibility = "visible";
+        restartGame.textContent = "Press the image to restart the game.";
+    }
+}
